69f73245859137561f80813bfd1050b34ca611a1
jpcww <jpcww@naver.com>
2018-08-17 00:53:38 -0400

    heart collecting, rotation, sound

---
+++ Assets/Scripts/HeartCollect.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartCollect : MonoBehaviour
+{
+    public int rotateSpeed;
+    public AudioSource heartCollectSount;
+    public GameObject heart;
+
+	// Update is called once per frame
+	void Update ()
+    {
+		transform.Rotate(0, rotateSpeed, 0, Space.World);
+	}
+
+    // collecting hearts
+    private void OnTriggerEnter(Collider other)
+    {
+        heartCollectSount.Play();
+        heart.SetActive(false);
+    }
+}
