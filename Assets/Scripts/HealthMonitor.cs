20f7264abd8f1e322c0fea270cbb2cfaf5b77b5e
jpcww <jpcww@naver.com>
2018-08-21 23:33:20 -0400

    05. HUD, UI, Environment(water)

---
+++ Assets/Scripts/HealthMonitor.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthMonitor : MonoBehaviour {
+
+    public static int heartValue;
+    public int internalHeart;
+    public GameObject heart1;
+    public GameObject heart2;
+    public GameObject heart3;
+
+    // Use this for initialization
+    void Start ()
+    {
+        heartValue = 1;
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        internalHeart = heartValue;
+
+        if (heartValue == 1)
+        {
+            heart1.SetActive(true);
+        }
+
+        if (heartValue == 2)
+        {
+            heart2.SetActive(true);
+        }
+
+        if(heartValue==3)
+        {
+            heart3.SetActive(true);
+        }
+
+	}
+}
--- Assets/Scripts/HeartCollect.cs
+++ Assets/Scripts/HeartCollect.cs
@@ -11,13 +11,18 @@ public class HeartCollect : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
+        // keep the heart rotating
 		transform.Rotate(0, rotateSpeed, 0, Space.World);
 	}
 
     // collecting hearts
     private void OnTriggerEnter(Collider other)
     {
+        // Play the sound when collecting a heart
         heartCollectSount.Play();
+        // Increase the heart by 1
+        HealthMonitor.heartValue += 1;
+        // make the heart disappear
         heart.SetActive(false);
     }
 }
