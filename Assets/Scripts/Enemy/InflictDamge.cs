a5ebb839bccd5f2e7973b130edcbb166451d82a6
jpcww <jpcww@naver.com>
2018-09-02 03:13:47 -0400

    Enemy & Inflicting Damage

---
+++ Assets/Scripts/Enemy/InflictDamge.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InflictDamge : MonoBehaviour
+{
+
+    public int damageAmount = 5;
+    public float targetDistance;
+    public float allowedRange = 2.7f;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit))
+            {
+                targetDistance = hit.distance;
+                if (targetDistance <= allowedRange)
+                {
+                    hit.transform.SendMessage("DeductPoints", damageAmount, SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+		
+	}
+}
---
+++ Assets/Scripts/Enemy/SpiderEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderEnemy : MonoBehaviour
+{
+    public int enemyHealth = 10;
+
+    void DeductPoints(int damageAmount)
+    {
+        enemyHealth -= damageAmount;
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+        if (enemyHealth <= 0)
+        {
+            StartCoroutine(DeathSpider());
+        }
+	}
+
+    // give a dealy to the death of Spider
+    IEnumerator DeathSpider()
+    {
+        yield return new WaitForSeconds(0.8f);
+        Destroy(gameObject);
+    }
+}
