e1a3cb0474ede549f2e80e8e7f507ed78bb3d1a4
jpcww <jpcww@naver.com>
2018-09-02 19:52:33 -0400

    12. Death Animation & Objtainning EXP

--- Assets/Scripts/Enemy/SpiderEnemy.cs
+++ Assets/Scripts/Enemy/SpiderEnemy.cs
@@ -5,24 +5,37 @@ using UnityEngine;
 public class SpiderEnemy : MonoBehaviour
 {
     public int enemyHealth = 10;
+    public GameObject spider;
+    public int spiderStatus;
+    public int baseEXP = 10;
+    public int calculatedEXP; 
 
+    // deduct the health of spider when it is hit
     void DeductPoints(int damageAmount)
     {
         enemyHealth -= damageAmount;
     }
+
 	// Update is called once per frame
 	void Update ()
     {
         if (enemyHealth <= 0)
         {
-            StartCoroutine(DeathSpider());
+            if (spiderStatus == 0)
+            {
+                StartCoroutine(DeathSpider());
+            }
+            
         }
 	}
 
     // give a dealy to the death of Spider
     IEnumerator DeathSpider()
     {
-        yield return new WaitForSeconds(0.8f);
-        Destroy(gameObject);
+        spiderStatus = 6;
+        calculatedEXP = baseEXP * GlobalLevel.currentLevel;
+        GlobalEXP.currentEXP += 10 + calculatedEXP;
+        yield return new WaitForSeconds(0.5f);
+        spider.GetComponent<Animation>().Play("die");
     }
 }
---
+++ Assets/Scripts/Experience/GlobalLevel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalLevel : MonoBehaviour
+{
+    public static int currentLevel = 2;
+    public int internalLevel;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        internalLevel = currentLevel;	
+	}
+}
