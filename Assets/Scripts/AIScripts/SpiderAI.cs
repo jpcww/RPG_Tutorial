7e301b1e53ca889c486f598fa0cf3dbfcdc54ece
jpcww <jpcww@naver.com>
2018-09-03 02:16:24 -0400

    13. Enemy AI

---
+++ Assets/Scripts/AIScripts/SpiderAI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderAI : MonoBehaviour
+{
+    public GameObject Player;
+    public float targetDistance;
+    public float allowedRange = 40;
+    public GameObject spider;
+    public float spiderSpeed;
+    public int attackTrigger;
+    public RaycastHit shot;
+
+	// Update is called once per frame
+	void Update ()
+    {
+        // look at Player
+        transform.LookAt(Player.transform);
+
+        // raycasting
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out shot))
+        {
+            targetDistance = shot.distance;
+
+            // when Player is within the range of Spider
+            if (targetDistance <= allowedRange)
+            {
+                spiderSpeed = 0.05f;
+
+                // the first state of Spider, Walking towards Player
+                if (attackTrigger == 0)
+                {
+                    spider.GetComponent<Animation>().Play("walk");
+                    transform.position = Vector3.MoveTowards(transform.position, Player.transform.position, spiderSpeed);
+                }
+            }
+
+            // when Player is out of the range of Spider,
+            // Spider idling
+            else
+            {
+                spiderSpeed = 0;
+                spider.GetComponent<Animation>().Play("idle");
+            }
+        }
+
+        // when the trigger is 1, spider stops and attacks
+        if (attackTrigger == 1)
+        {
+            spiderSpeed = 0;
+            spider.GetComponent<Animation>().Play("attack");
+        }
+	}
+
+    // when spider reaches Player's colldier
+    void OnTriggerEnter(Collider other)
+    {
+        attackTrigger = 1;
+
+    }
+
+    // when spider doesn't lose Player's collider
+    void OnTriggerExit()
+    {
+        attackTrigger = 0;
+    }
+}
---
+++ Assets/Scripts/NPCInteraction/NPC001.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NPC001 : MonoBehaviour
+{
+    public float distance;
+    public GameObject actionButton;
+    public GameObject actionText;
+    public GameObject player;
+    public GameObject textBox;
+    public GameObject NPCName;
+    public GameObject NPCText;
+
+
+    private void Update()
+    {
+        // get the distance from gameObject and Player
+        distance = PlayerCasting.distanceFromTarget;
+    }
+
+    // when mouse is on the NPC
+    private void OnMouseOver()
+    {
+        // when the distance is less than 3
+        if (distance <=3)
+        {
+            AttackBlocker.blockSword = 1;
+            actionText.GetComponent<Text>().text = "Talk";
+            actionText.SetActive(true);
+            actionButton.SetActive(true);
+
+            // when action button is press
+            if (Input.GetButtonDown("Action"))
+            {
+                // block attacking
+                AttackBlocker.blockSword = 2;
+
+                //let mouse function
+                Screen.lockCursor = false;
+                Cursor.visible = true;
+
+                // turn off UIs
+                actionText.SetActive(false);
+                actionButton.SetActive(false);
+
+                // get couroutine started
+                StartCoroutine(NPC001Active());
+            }
+        }
+    }
+
+    // get NPC001 interact with Player
+    IEnumerator NPC001Active()
+    {
+        // turn on UIs for interacting
+        textBox.SetActive(true);
+        NPCName.GetComponent<Text>().text = "Red";
+        NPCName.SetActive(true);
+        NPCText.GetComponent<Text>().text = "Greetings, I need your help. If you wish to, please come to me here in the afternoon";
+        NPCText.SetActive(true);
+
+        // wait for 5 sec for the user to read
+        yield return new WaitForSeconds(5);
+
+        // turn off UIs for interacting
+        NPCName.SetActive(false);
+        NPCText.SetActive(false);
+        textBox.SetActive(false);
+
+        // get the premier UIs back
+        actionButton.SetActive(true);
+        actionText.SetActive(true);
+        
+    }
+
+    // when mouse has exited from the NPC
+    private void OnMouseExit()
+    {
+        // attacking functioning
+        AttackBlocker.blockSword = 0;
+
+        // turn off UIs
+        actionText.SetActive(false);
+        actionButton.SetActive(false);
+    }
+
+
+}
