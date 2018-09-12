1014f5cdb6862089077970e1ae9faae739549a65
jpcww <jpcww@naver.com>
2018-09-11 23:58:24 -0400

    19. NavMesh & Boss AI

---
+++ Assets/Scripts/AIScripts/SpiderBossAI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SpiderBossAI : MonoBehaviour
+{
+
+    public GameObject destination;
+    NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Start ()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        navMeshAgent.SetDestination(destination.transform.position);
+		
+	}
+}
---
+++ Assets/Scripts/AIScripts/SpiderBossAttack.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderBossAttack : MonoBehaviour
+{
+    public GameObject enemy;
+    public int attackTrigger;
+    public int dealDamage;
+
+
+	// Update is called once per frame
+	void Update ()
+    {
+        if(attackTrigger == 0)
+        {
+            enemy.GetComponent<Animation>().Play("Walk");
+        }
+
+        if(attackTrigger == 1)
+        {
+            if(dealDamage == 0)
+            {
+                enemy.GetComponent<Animation>().Play("Attack");
+                StartCoroutine(TakingDamage());
+            }
+        }
+		
+	}
+
+    IEnumerator TakingDamage()
+    {
+        dealDamage = 2;
+        yield return new WaitForSeconds(1.1f);
+
+        if(SpiderEnemy.globalSpider !=6)
+        {
+            HealthMonitor.heartValue -= 1;
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        dealDamage = 0;
+    }
+
+    void OnTriggerEnter()
+    {
+        attackTrigger = 1;
+    }
+
+    void OnTriggerExit()
+    {
+        attackTrigger = 0;
+    }
+}
