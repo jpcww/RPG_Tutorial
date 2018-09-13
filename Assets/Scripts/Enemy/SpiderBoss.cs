e10b8d349f2038fb1ed16101f29d327809a1a9ea
jpcww <jpcww@naver.com>
2018-09-13 00:20:27 -0400

    20. Boss Dying & Open Gate

---
+++ Assets/Scripts/Enemy/SpiderBoss.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiderBoss : MonoBehaviour
+{
+    public int enemyHealth = 5;
+    public GameObject spider;
+    public int spiderStatus;
+    public int baseEXP = 10;
+    public int calculatedEXP;
+    public SpiderBossAI spiderBossAI;
+    public static int globalSpider;
+    public GameObject oldNPC;
+    public GameObject newNPC;
+    public SpiderBossAttack spiderBossAttack;
+
+    private void Start()
+    {
+        spiderBossAI = GetComponent<SpiderBossAI>();
+        spiderBossAttack = GetComponent<SpiderBossAttack>();
+    }
+
+    // deduct the health of spider when it is hit
+    void DeductPoints(int damageAmount)
+    {
+        enemyHealth -= damageAmount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // make the spider's status global(static)
+        globalSpider = spiderStatus;
+
+        if (enemyHealth <= 0)
+        {
+            if (spiderStatus == 0)
+            {
+                StartCoroutine(DeathSpider());
+            }
+
+        }
+    }
+
+    // give a dealy to the death of Spider
+    IEnumerator DeathSpider()
+    {
+        // turn off spiderAI and SPiderAttack component
+        spiderBossAI.enabled = false;
+        spiderBossAttack.enabled = false;
+
+        // change the indicator to 6
+        // obtain EXP
+        spiderStatus = 6;
+        calculatedEXP = baseEXP * GlobalLevel.currentLevel;
+        GlobalEXP.currentEXP += 10 + calculatedEXP;
+        yield return new WaitForSeconds(0.5f);
+
+        // play death animation
+        spider.GetComponent<Animation>().Play("Death");
+        yield return new WaitForSeconds(0.3f);
+
+        // turn off animation component
+        spider.GetComponent<Animation>().enabled = false;
+
+        oldNPC.SetActive(false);
+        newNPC.SetActive(true);
+    }
+}
+
---
+++ Assets/Scripts/Quest/Quest001/Q001OpenGate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Q001OpenGate : MonoBehaviour
+{
+    public float distance;
+    public GameObject actionButton;
+    public GameObject actionText;
+    public GameObject leftGate;
+    public GameObject rightGate;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        distance = PlayerCasting.distanceFromTarget;
+	}
+
+    private void OnMouseOver()
+    {
+        if(distance <= 3)
+        {
+            AttackBlocker.blockSword = 1;
+            actionText.GetComponent<Text>().text = "Open Gate";
+            actionText.SetActive(true);
+            actionButton.SetActive(true);
+
+
+            if (Input.GetButtonDown("Action"))
+            {
+                // when action button is pressed, 
+                //turn off the collider
+                GetComponent<BoxCollider>().enabled = false;
+
+                // prevent player from attacking
+                AttackBlocker.blockSword = 2;
+
+                // turn off uis
+                // play animation of gate to open them
+                actionButton.SetActive(false);
+                actionText.SetActive(false);
+                actionText.GetComponent<Text>().text = " ";
+                leftGate.GetComponent<Animation>().Play("LeftGate");
+                rightGate.GetComponent<Animation>().Play("RightGate");
+            }
+        }
+    }
+}
